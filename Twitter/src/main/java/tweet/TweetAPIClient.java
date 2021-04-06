f6d45b1f7a6fa503e8423e9aa071970c5a9d9666
mamiar1989 <mariaamiar89@gmail.com>
2021-04-06 12:39:02 -0400

    updates

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -29,7 +29,7 @@ public class TweetAPIClient extends RestAPI {
     public ValidatableResponse createTweet(String tweet) {
         return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
                 .param("status", tweet)
-                .when().post(this.baseUrl + this.CREATE_TWEET_ENDPOINT).then().statusCode(200);
+                .when().post(this.baseUrl + this.CREATE_TWEET_ENDPOINT).then();
     }
 
     // Delete Tweet
@@ -40,81 +40,42 @@ public class TweetAPIClient extends RestAPI {
 
     }
 
+    // Delete a tweet from user twitter
+    public ValidatableResponse deleteTweet(Long tweetId){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+                .queryParam("id",tweetId)
+                .when().delete(this.baseUrl+this.DELETE_TWEET_ENDPOINT).then().statusCode(200);
+    }
+
+
+    // Response Time check
+    public ValidatableResponse responseTime(){
+        System.out.println(given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
+                .timeIn(TimeUnit.MILLISECONDS));
+        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
+                .then();
+
+    }
+
+    // Header Value
+    public void headerValue(){
+        System.out.println(given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
+                .then().extract().headers());
+
+    }
+
+    public  void checkProperty(){
+        Response response= given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT);
+        JsonPath pathEvaluator= response.jsonPath();
+        String createdAt=pathEvaluator.get("id");
+        System.out.println(createdAt);
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    private final String CREATE_TWEET_ENDPOINT="/statuses/update.json";
-//    private final String DELETE_TWEET_ENDPOINT="/statuses/destroy.json";
-//    private final String GET_USER_TWEET_ENDPOINT="/statuses/home_timeline.json";
-//
-////    // GET all Tweet Information
-//    public ValidatableResponse getUserTimeTweet(){
-//        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
-//                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT).then().statusCode(200);
-//    }
-//
-//    // Create a Tweet from user twitter
-//    public ValidatableResponse createTweet(String tweet){
-//        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-//                .param("status",tweet)
-//                .when().post(this.baseUrl+this.CREATE_TWEET_ENDPOINT)
-//                .then();
-//    }
-//
-//    // Delete a tweet from user twitter
-//    public ValidatableResponse deleteTweet(Long tweetId){
-//        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
-//                .queryParam("id",tweetId)
-//                .when().delete(this.baseUrl+this.DELETE_TWEET_ENDPOINT).then().statusCode(200);
-//    }
-//
-//
-//    // Response Time check
-//    public ValidatableResponse responseTime(){
-//        System.out.println(given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-//                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
-//                .timeIn(TimeUnit.MILLISECONDS));
-//        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-//                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
-//                .then();
-//
-//    }
-//
-//    // Header Value
-//    public void headerValue(){
-//        System.out.println(given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-//                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT)
-//                .then().extract().headers());
-//
-//    }
-//
-//    public  void checkProperty(){
-//        Response response= given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
-//                .when().get(this.baseUrl+this.GET_USER_TWEET_ENDPOINT);
-//        JsonPath pathEvaluator= response.jsonPath();
-//        String createdAt=pathEvaluator.get("id");
-//        System.out.println(createdAt);
-//    }
-//
-//
 //    // OAuth
 //    // https://www.programcreek.com/java-api-examples/?api=com.github.scribejava.core.model.OAuthRequest
 //
--- Twitter/src/test/java/tweettest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweettest/TweetAPIClientTest.java
@@ -21,7 +21,21 @@ public class TweetAPIClientTest {
     @Test
     public void testUserCanTweetSuccessfully() {
         // User sent a tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet" + UUID.randomUUID().toString();
+        String tweet = "We are learning Rest API using Rest Assured and our this is my First Tweet" + UUID.randomUUID().toString();
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        System.out.println(response.extract().body().asPrettyString());
+        // Verify that the tweet is successful
+        response.statusCode(200);
+        // Verity tweet value
+        String actualTweet = response.extract().body().path("text");
+        Long id = response.extract().body().path("id");
+        System.out.println(id);
+        Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
+    }
+
+    public void testUserCanTweetSuccessfully3() {
+        // User sent a tweet
+        String tweet = "Today is My Day " + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         System.out.println(response.extract().body().asPrettyString());
         // Verify that the tweet is successful
@@ -36,9 +50,8 @@ public class TweetAPIClientTest {
 
     @Test
     public void testUserCanNotTweetTwiceInARow() {
-
         // User sent a tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet Same Tweet";
+        String tweet = "We are learning Rest API using Rest Assured and our First Tweet87dc3c71-a296-468b-baf3-1ac36f1e47e5";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         // Verify that the tweet is successful
         response.statusCode(403);
@@ -46,113 +59,85 @@ public class TweetAPIClientTest {
         System.out.println(response.extract().body().asPrettyString());
         String expectedMessage = "Status is a duplicate.";
         String actualTweet = response.extract().body().path("errors[0].message");
-        Assert.assertEquals(actualTweet, expectedMessage, "Tweet is match");
-        Assert.assertNotEquals("403", 200);
+       Assert.assertEquals(actualTweet, expectedMessage, "Tweet is match");
+       Assert.assertNotEquals("403", 200);
+    }
+
+    @Test
+    public void testUserCanTweetSuccessfully2() {
+        // User sent a tweet
+        String tweet = "Hola! "+ UUID.randomUUID().toString();
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        System.out.println(response.extract().body().asPrettyString());
+        // Verify that the tweet is successful
+        response.statusCode(200);
+        // Verity tweet value
+        String actualTweet=response.extract().body().path("text");
+        Long id= response.extract().body().path("id");
+        System.out.println(id);
+        Assert.assertNotEquals("id","text");
     }
 
 
+    @Test//create tweet-3 passed
+    public void testUserCanTweet() {
+        // User sent a tweet
+        String tweet = "Bootcamp project is so stressful "+ UUID.randomUUID().toString();
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        response.statusCode(200);
+        String actualTweet=response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet,"Tweet don't match");
+    }
+    @Test//test-4-failed
+    public void testUserCanDeleteTweet1(){
+        String tweet="We are learning Rest API using Rest Assured and our First Tweet295ba966-1e9a-4628-a8bb-e1a70f6bf8a0";
+        ValidatableResponse deleteResponse1=this.tweetAPIClient.DeleteTweet(1378047777687678985l);
+        deleteResponse1.statusCode(200);
+        String actualTweet=deleteResponse1.extract().body().path("text");
+        Assert.assertEquals(tweet,actualTweet);
+    }
+    @Test
+    public void testUserCanNotTweetTheSameTweetTwiceInARow() {
+        // User sent a tweet
+        String tweet = "We are learning Rest API using Rest Assured and our Tweet is same tweet";
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        // Verify that the tweet is successful
+        //response.statusCode(403);
+        // Verity Retweet
+        System.out.println(response.extract().body().asPrettyString());
+        String expectedMessage="Status is a duplicate.";
+        String actualTweet=response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualTweet,expectedMessage,"Tweet match");
+        Assert.assertNotEquals("403",200);
+    }
 
 
+    @Test(enabled = false)
+    public void testResponseTime() {
+        ValidatableResponse response = this.tweetAPIClient.responseTime();
+    }
+    @Test(enabled = false)
+    public void testHeaderValue() {
+        this.tweetAPIClient.headerValue();
+    }
 
+    @Test(enabled = true)
+    public void testPropertyFromResponse() {
+        //1. User send a tweet
+        String tweet = "We are learning Rest API Automation with restApiAutomation_Final_" +
+                "the hard work is coming yall!!!!!" + UUID.randomUUID().toString();
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        //2. Verify that the tweet was successful
+        // response.statusCode(200);
 
+        //this.tweetAPIClient.checkProperty();
+        //JsonPath pathEvaluator=response.;
+        //System.out.println(response.extract().body().asPrettyString());
+        System.out.println(response.extract().body().asPrettyString().contains("id"));
 
-
-
-
-
-
-//    @BeforeClass
-//    public void setUpTweetAPI() {
-//        this.tweetAPIClient = new TweetAPIClient();
-//    }
-//
-//    @Test//create tweet-1 passed
-//    public void testUserCanTweetSuccessfully() {
-//        // User sent a tweet
-//        String tweet = "We are learning Rest API using Rest Assured and our First Tweet";
-//        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-//        // Verify that the tweet is successful
-//        response.statusCode(200);
-//        // Verify tweet value
-//        String actualTweet=response.extract().body().path("text");
-//        Assert.assertEquals(actualTweet,tweet,"Tweet don't match");
-//    }
-//
-//
-//    @Test//create tweet-2 passed
-//    public void testUserCanTweetSuccessfully2() {
-//        // User sent a tweet
-//        String tweet = "We are learning Rest API using Rest Assured and our First Tweet"+ UUID.randomUUID().toString();
-//        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-//        System.out.println(response.extract().body().asPrettyString());
-//        // Verify that the tweet is successful
-//        response.statusCode(200);
-//        // Verity tweet value
-//        String actualTweet=response.extract().body().path("text");
-//        Long id= response.extract().body().path("id");
-//        System.out.println(id);
-//        Assert.assertNotEquals("id","text");
-//    }
-//    @Test//create tweet-3 passed
-//    public void testUserCanTweet() {
-//        // User sent a tweet
-//        String tweet = "Bootcamp project is so stressful "+ UUID.randomUUID().toString();
-//        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-//        response.statusCode(200);
-//        String actualTweet=response.extract().body().path("text");
-//        Assert.assertEquals(actualTweet,tweet,"Tweet don't match");
-//    }
-//    @Test//test-4-failed
-//    public void testUserCanDeleteTweet(){
-//        String tweet="Bootcamp project is so stressful d933fd25-e040-4b3c-a285-d07d7b05b78c";
-//        ValidatableResponse deleteResponse1=this.tweetAPIClient.deleteTweet(1378047777687678985l);
-//        deleteResponse1.statusCode(200);
-//        String actualTweet=deleteResponse1.extract().body().path("text");
-//        Assert.assertEquals(tweet,actualTweet);
-//    }
-//
-//
-//    @Test //5-passed
-//    public void testUserCanNotTweetTheSameTweetTwiceInARow() {
-//        // User sent a tweet
-//        String tweet = "We are learning Rest API using Rest Assured and our Tweet is same tweet";
-//        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-//        // Verify that the tweet is successful
-//        //response.statusCode(403);
-//        // Verity Retweet
-//        System.out.println(response.extract().body().asPrettyString());
-//        String expectedMessage="Status is a duplicate.";
-//        String actualTweet=response.extract().body().path("errors[0].message");
-//        Assert.assertEquals(actualTweet,expectedMessage,"Tweet match");
-//        Assert.assertNotEquals("403",200);
-//    }
-//
-
-//    @Test(enabled = false)
-//    public void testResponseTime() {
-//        ValidatableResponse response = this.tweetAPIClient.responseTime();
-//    }
-//    @Test(enabled = false)
-//    public void testHeaderValue() {
-//        this.tweetAPIClient.headerValue();
-//    }
-//
-//    @Test(enabled = false)
-//    public void testPropertyFromResponse() {
-//        //1. User send a tweet
-//        String tweet = "We are learning Rest API Automation with restApiAutomation_Final_Farhana" + UUID.randomUUID().toString();
-//        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-//        //2. Verify that the tweet was successful
-//        // response.statusCode(200);
-//
-//        //this.tweetAPIClient.checkProperty();
-//        //JsonPath pathEvaluator=response.;
-//        //System.out.println(response.extract().body().asPrettyString());
-//        System.out.println(response.extract().body().asPrettyString().contains("id"));
-//
-//        //String actualTweet = response.extract().body().path("text");
-//        //Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
-//    }
+        //String actualTweet = response.extract().body().path("text");
+        //Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
+    }
 
 
 
