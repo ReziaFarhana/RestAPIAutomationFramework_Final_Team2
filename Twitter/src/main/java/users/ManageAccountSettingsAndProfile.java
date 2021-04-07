93114c2698f26500384dea9f6a703a31b5e9745e
isratreto4395 <isratreto4395@gmail.com>
2021-04-06 22:58:21 -0500

    25/50

--- Twitter/src/main/java/users/ManageAccountSettingsAndProfile.java
+++ Twitter/src/main/java/users/ManageAccountSettingsAndProfile.java
@@ -8,6 +8,8 @@ import static io.restassured.RestAssured.*;
 public class ManageAccountSettingsAndProfile extends RestAPI {
     private final String GET_ACCOUNT_SETTINGS_ENDPOINT = "/account/settings.json";
     private final String GET_ACCOUNT_VERIFY_CREDENTIALS_ENDPOINT = "/account/verify_credentials.json";
+    private final String GET_USERS_PROFILE_BANNER_ENDPOINT = "/users/profile_banner.json";
+    private final String POST_ACCOUNT_REMOVE_PROFILE_BANNER_ENDPOINT = "/account/remove_profile_banner.json";
 
     //returns user settings
     public ValidatableResponse returnUserSettings() {
@@ -16,5 +18,45 @@ public class ManageAccountSettingsAndProfile extends RestAPI {
                 .then();
     }
 
-    //
+    //Check whether user credential is valid
+    public ValidatableResponse verifyCredential(String name, boolean includeEmail) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("name",name).param("include_email",includeEmail)
+                .when().get(this.baseUrl + this.GET_ACCOUNT_VERIFY_CREDENTIALS_ENDPOINT)
+                .then();
+    }
+
+    //Check user's profile banner
+    public ValidatableResponse verifyProfileBanner(String screenName) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("screen_name",screenName)
+                .when().get(this.baseUrl + this.GET_USERS_PROFILE_BANNER_ENDPOINT)
+                .then();
+    }
+
+    //Removes the uploaded profile banner
+    public ValidatableResponse removeProfileBanner() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.POST_ACCOUNT_REMOVE_PROFILE_BANNER_ENDPOINT)
+                .then().statusCode(200);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Twitter/src/test/java/userstest/ManageAccountSettingsAndProfileTest.java
+++ Twitter/src/test/java/userstest/ManageAccountSettingsAndProfileTest.java
@@ -19,10 +19,52 @@ public class ManageAccountSettingsAndProfileTest {
     public void testUserCanGetUserSettings() {
         String expectedScreenName = "IsratReto";
         ValidatableResponse response = this.accountSettingsAndProfile.returnUserSettings();
-        System.out.println(response.extract().body().asPrettyString());
+        //System.out.println(response.extract().body().asPrettyString());
         String actualScreenName = response.extract().body().path("screen_name");
         Assert.assertEquals(actualScreenName, expectedScreenName, "Wrong screen name");
     }
+
+    @Test //2
+    public void testUserCanVerifyCredential(){
+        String expectedName = "Israt Reto";
+        ValidatableResponse response = this.accountSettingsAndProfile.verifyCredential("Israt Reto",true);
+        //System.out.println(response.extract().body().asPrettyString());
+        String actualName = response.extract().body().path("name");
+        Assert.assertEquals(actualName, expectedName, "Wrong name");
+    }
+
+    @Test //3
+    public void testUserCannotVerifyProfileBanner(){
+        String expectedErrorMessage = "Sorry, that page does not exist";
+        ValidatableResponse response = this.accountSettingsAndProfile.verifyProfileBanner("IsratReto");
+        System.out.println(response.extract().body().asPrettyString());
+        String actualErrorMessage = response.extract().body().path("errors[0].message");
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage, "No error message");
+    }
+
+    @Test //3
+    public void testUserCanVerifyProfileBanner(){
+        int heightOfProfilePictureOfWeb = 260;
+        ValidatableResponse response = this.accountSettingsAndProfile.verifyProfileBanner("IsratReto");
+//        System.out.println(response.extract().body().asPrettyString());
+        int actualHeightOfProfilePictureOfWeb = response.extract().body().path("sizes.web.h");
+        Assert.assertEquals(actualHeightOfProfilePictureOfWeb, heightOfProfilePictureOfWeb, "Wrong height");
+    }
+
+    @Test //4
+    public void testUserCanremoveProfileBanner(){
+        ValidatableResponse response = this.accountSettingsAndProfile.removeProfileBanner();
+        System.out.println(response.extract().body().asPrettyString());
+
+    }
+
+
+
+
+
+
+
+
 
 
 
