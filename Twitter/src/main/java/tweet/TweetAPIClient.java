306135601c12dea1fd18edd72baf1f9738c3e6d9
mamiar1989 <mariaamiar89@gmail.com>
2021-04-10 12:22:01 -0400

    updates

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -15,6 +15,11 @@ public class TweetAPIClient extends RestAPI {
     private final String CREATE_TWEET_ENDPOINT = "/statuses/update.json";
     private final String DELETE_TWEET_ENDPOINT = "/statuses/destroy.json";
     private final String GET_USER_TWEET_ENDPOINT = "/statuses/home_timeline.json";
+    private final String GET_USER_STATUS_SHOW="/statuses/show.json";
+    private final String POST_USER_RETWEET_ENDOINT="statuses/retweet/:id.json";
+
+
+
 
     // to Get all tweet information
 
@@ -74,6 +79,18 @@ public class TweetAPIClient extends RestAPI {
         String createdAt=pathEvaluator.get("id");
         System.out.println(createdAt);
     }
+
+    // Create a Tweet from user twitter
+    public ValidatableResponse showStatus(long tweetid){
+        return given().auth().oauth(this.apiKey,this.apiSecretKey, this.accessToken,this.accessTokenSecret)
+                .param("id",tweetid)
+                .when().get(this.baseUrl+this.GET_USER_STATUS_SHOW)
+                .then();
+    }
+
+
+
+
 
 }
 //    // OAuth
--- Twitter/src/test/java/tweettest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweettest/TweetAPIClientTest.java
@@ -18,15 +18,12 @@ public class TweetAPIClientTest {
         this.tweetAPIClient = new TweetAPIClient();
     }
 
-    @Test
+    @Test //Tweet number 1
     public void testUserCanTweetSuccessfully() {
-        // User sent a tweet
-        String tweet = "We are learning Rest API using Rest Assured and our this is my First Tweet" + UUID.randomUUID().toString();
+        String tweet = "I am learning Rest API This is my First Tweet . " + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         System.out.println(response.extract().body().asPrettyString());
-        // Verify that the tweet is successful
         response.statusCode(200);
-        // Verity tweet value
         String actualTweet = response.extract().body().path("text");
         Long id = response.extract().body().path("id");
         System.out.println(id);
@@ -34,13 +31,11 @@ public class TweetAPIClientTest {
     }
 @Test
     public void testUserCanTweetSuccessfully3() {
-        // User sent a tweet
-        String tweet = "Today is My Day " + UUID.randomUUID().toString();
+
+        String tweet = "Today is a Beautiful Day " + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         System.out.println(response.extract().body().asPrettyString());
-        // Verify that the tweet is successful
         response.statusCode(200);
-        // Verity tweet value
         String actualTweet = response.extract().body().path("text");
         Long id = response.extract().body().path("id");
         System.out.println(id);
@@ -50,12 +45,9 @@ public class TweetAPIClientTest {
 
     @Test
     public void testUserCanNotTweetTwiceInARow() {
-        // User sent a tweet
-        String tweet = "We are learning Rest API using Rest Assured and our First Tweet87dc3c71-a296-468b-baf3-1ac36f1e47e5";
+        String tweet = "This is Two Tweets in a Row";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-        // Verify that the tweet is successful
         response.statusCode(403);
-        // Verity Retweet
         System.out.println(response.extract().body().asPrettyString());
         String expectedMessage = "Status is a duplicate.";
         String actualTweet = response.extract().body().path("errors[0].message");
@@ -65,80 +57,83 @@ public class TweetAPIClientTest {
 
     @Test
     public void testUserCanTweetSuccessfully2() {
-        // User sent a tweet
         String tweet = "Hola! "+ UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         System.out.println(response.extract().body().asPrettyString());
-        // Verify that the tweet is successful
         response.statusCode(200);
-        // Verity tweet value
         String actualTweet=response.extract().body().path("text");
         Long id= response.extract().body().path("id");
         System.out.println(id);
         Assert.assertNotEquals("id","text");
     }
-
-
-    @Test//create tweet-3 passed
+    @Test
     public void testUserCanTweet() {
-        // User sent a tweet
-        String tweet = "Bootcamp project is so stressful "+ UUID.randomUUID().toString();
+        String tweet = "I need a vacation ! "+ UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         response.statusCode(200);
         String actualTweet=response.extract().body().path("text");
         Assert.assertEquals(actualTweet,tweet,"Tweet don't match");
     }
-    @Test//test-4-failed
+    @Test
     public void testUserCanDeleteTweet1(){
-        String tweet="We are learning Rest API using Rest Assured and our First Tweet295ba966-1e9a-4628-a8bb-e1a70f6bf8a0";
-        ValidatableResponse deleteResponse1=this.tweetAPIClient.DeleteTweet(1378047777687678985l);
+        String tweet="";
+        ValidatableResponse deleteResponse1=this.tweetAPIClient.DeleteTweet(1380914752621318145L);
         deleteResponse1.statusCode(200);
         String actualTweet=deleteResponse1.extract().body().path("text");
         Assert.assertEquals(tweet,actualTweet);
     }
     @Test
     public void testUserCanNotTweetTheSameTweetTwiceInARow() {
-        // User sent a tweet
         String tweet = "We are learning Rest API using Rest Assured and our Tweet is same tweet";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-        // Verify that the tweet is successful
-        //response.statusCode(403);
-        // Verity Retweet
         System.out.println(response.extract().body().asPrettyString());
         String expectedMessage="Status is a duplicate.";
         String actualTweet=response.extract().body().path("errors[0].message");
         Assert.assertEquals(actualTweet,expectedMessage,"Tweet match");
         Assert.assertNotEquals("403",200);
     }
-
-
-    @Test(enabled = false)
-    public void testResponseTime() {
-        ValidatableResponse response = this.tweetAPIClient.responseTime();
-    }
-    @Test(enabled = false)
-    public void testHeaderValue() {
-        this.tweetAPIClient.headerValue();
-    }
-
     @Test(enabled = true)
     public void testPropertyFromResponse() {
-        //1. User send a tweet
         String tweet = "We are learning Rest API Automation with restApiAutomation_Final_" +
                 "the hard work is coming yall!!!!!" + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-        //2. Verify that the tweet was successful
-        // response.statusCode(200);
-
-        //this.tweetAPIClient.checkProperty();
-        //JsonPath pathEvaluator=response.;
-        //System.out.println(response.extract().body().asPrettyString());
         System.out.println(response.extract().body().asPrettyString().contains("id"));
-
-        //String actualTweet = response.extract().body().path("text");
-        //Assert.assertEquals(actualTweet, tweet, "Tweet is not match");
     }
 
+    @Test//test-4-failed
+    public void testUserCanDeleteTweet(){
+        String tweet=" I love Summer Time!";
+        ValidatableResponse deleteResponse1=this.tweetAPIClient.deleteTweet( 1380649917060939778l);
+        deleteResponse1.statusCode(200);
+        String actualTweet=deleteResponse1.extract().body().path("text");
+        Assert.assertEquals(tweet,actualTweet);
+    }
+
+    @Test
+    public void testUserIsableToCreateAnotherTweet(){
+        String tweet="I am practicing RestApi Tonight";
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        response.statusCode(200);
+        String actualTweet=response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet,"Tweets don't match");
+
+    }
+    @Test
+    public void userCanTweetTheMessageOnceMore(){
+        String tweet="Trying to tweet once more";
+        ValidatableResponse response=this.tweetAPIClient.createTweet(tweet);
+        response.statusCode(200);
+        String actualTweet=response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet,"tweet did not match");
+    }
+    @Test
+    public void userShouldDeleteTweet(){
+        String tweet="I am still working on API 8c5fcfc1-d502-4f52-985e-7da4a4580271";
+        ValidatableResponse response=this.tweetAPIClient.deleteTweet(1380700880329129989L);
+        response.statusCode(200);
+        String actuaTweet=response.extract().body().path("text");
+        Assert.assertEquals(actuaTweet,tweet,"Tweet did not match");
+    }
 
 
 }
